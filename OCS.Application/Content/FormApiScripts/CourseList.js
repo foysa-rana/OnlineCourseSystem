ac867f07a815115bb3c27adba6523bbbbf0c81bf
turjohasnat7815 <turjohasnat7815@gmail.com>
2023-12-11 10:46:53 +0600

    save, update, delete done at seminar & course

---
+++ OCS.Application/Content/FormApiScripts/CourseList.js
@@ -0,0 +1,63 @@
+﻿//get all data list
+let getListData = async () => {
+    let p = await fetch("/api/Course");
+    let data = await p.json();
+    let dataListTable = document.querySelector('#data-list-table');
+    let storeData = "";
+    data.forEach((item, index) => {
+        storeData +=
+            `<tr>
+                <td>${item.trainerId}</td>
+                <td>${item.name}</td>
+                <td>${item.duration}</td>
+                <td class="d-flex">
+                    <span class="bullet-active" id="status-bullet"></span>
+                    <span class="status">Active</span>
+                </td>
+                <td class="action-icon">
+                    <i class="fa-solid fa-ellipsis-vertical action-btn"></i>
+                    <span class="action">
+                        <a class="edit-btn" href="CreateTrainers?id=${item.id}"><i class="fa-solid fa-pen"></i></a>
+                        <a class="delete-btn" data-id="${item.id}"><i class="fa-solid fa-trash"></i></a>
+                        <a class="details-btn" href="#"><i class="fa-solid fa-circle-info"></i></a>
+                    </span>
+                </td>
+            </tr>`;
+    });
+    dataListTable.innerHTML = storeData;
+    //data list action button
+    let dataListAction = async () => {
+        let dataListBtn = await document.querySelectorAll('.action-btn');
+        let action = await document.querySelectorAll('.action');
+        dataListBtn.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                action.forEach(element => {
+                    element.classList.remove('action-show')
+                });
+                action[index].classList.add('action-show')
+
+            });
+        })
+    }
+    dataListAction();
+
+    //delete data
+
+}
+
+getListData();
+
+$(document.body).on("click", ".delete-btn", function () {
+    let id = $(this).attr("data-id");
+    $.ajax({
+        url: "/api/Course/" + id,
+        type: "DELETE",
+        success: () => {
+            getListData();
+        },
+        error: function (request, status, error) {
+            let response = jQuery.parseJSON(request.responseText);
+            console.log(response.message, "Error");
+        }
+    })
+})
---
+++ OCS.Application/Content/FormApiScripts/SeminarCreate.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(() => {
+    formValue();
+
+});
+//refresh form
+let refresh = () => {
+    $("#Id").val("");
+    $("#Photo").val("");
+    $("#TrainerId").val("");
+    $("#Name").val("");
+    $("#Duration").val("");
+    $("#Description").val("");
+}
+
+//update or post
+let formValue = () => {
+    // get id from url ==> array
+    getData();
+
+    $("#Submit").on('click', () => {
+        let vm = {};
+        let id = $("#Id").val();
+        vm.Photo = $("#Photo").val();
+        vm.TrainerId = $("#TrainerId").val();
+        vm.Name = $("#Name").val();
+        vm.Duration = $("#Time").val();
+        vm.Description = $("#Description").val();
+        //post formdata
+        if (id == 0 || id == null || id == '' || id == undefined) {
+            let apiUrl = "/api/Seminar";
+            postData(vm, apiUrl, refresh);
+        }
+        else {
+            vm.id = id;
+            let apiUrl = "/api/Seminar/" + id;
+            updateData(vm, apiUrl, refresh);
+        }
+    });
+}
+
+//get data
+let getData = () => {
+    let queryValue = getUrlVars();
+    let queryId = queryValue.id;
+    let url = "/api/Seminar?id=" + queryId;
+    if (queryId) {
+        $.get(url)
+            .done((data) => {
+                $("#Id").val(data.id);
+                $("#Photo").val(data.Photo);
+                $("#TrainerId").val(data.TrainerId);
+                $("#Name").val(data.Name);
+                $("#Time").val(data.Time);
+                $("#Description").val(data.Description);
+            })
+    }
+}
